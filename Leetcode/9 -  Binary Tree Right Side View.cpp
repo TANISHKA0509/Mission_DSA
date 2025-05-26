84f641801363134ef729fea7e86e6a9cda39860f
TANISHKA0509 <181134200+TANISHKA0509@users.noreply.github.com>
2025-05-26 06:59:31 +0530

    Added

---
+++ Leetcode/9 -  Binary Tree Right Side View.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        if (!root) return result;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int level_size = q.size();
+            for (int i = 0; i < level_size; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (i == level_size - 1) {
+                    result.push_back(node->val);
+                }
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+        return result;
+    }
+};
